1d0a0b5875a2defa961fe6c68e43a943ecf2a94c
kambe.kenta <kenta.k199405@gmail.com>
2021-02-11 17:42:38 +0900

    added useEffect function.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App(){
+  const [images, setImages] = useState([]);
+  const [text, setText] = useState('');
+  const [query, setQuery] = useState('apple');
+  useEffect(() => {
+    console.log('useEffect passed.')
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setImages(data.results)
+        })
+  }, [query])
   return  (
     <div>
 
